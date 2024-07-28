93d86da5cceb6eec76c3022357db8ffc1baf30b4
roberto-fernandino <romfernandino@gmail.com>
2024-07-28 13:32:32 -0300

    feat: post install script to configure the machine of the user.

---
+++ src/aicli100/post_install.py
@@ -0,0 +1,13 @@
+import os
+import configparser
+
+def create_config():
+    config = configparser.ConfigParser()
+    config["API_KEYS"] = {"OPEN_AI_API_KEY": "", "ANTHROPIC_API_KEY": ""}
+    config["USER_CFG"] = {"FIRST_USE": 1}
+    config["AI"] = {"ai": 'None'}
+    home_dir = os.path.expanduser("~")
+    config_file = os.path.join(home_dir, ".aicli_config.ini")
+    with open(config_file, "w") as configfile:
+        config.write(configfile)
+    
