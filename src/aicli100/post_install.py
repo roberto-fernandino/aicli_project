9d4a0351dbf8f49612490f7b73f71d2a4196cd2f
roberto-fernandino <romfernandino@gmail.com>
2024-07-28 13:32:48 -0300

    feat: Fixed USER_CFG area.

--- src/aicli100/post_install.py
+++ src/aicli100/post_install.py
@@ -4,8 +4,7 @@ import configparser
 def create_config():
     config = configparser.ConfigParser()
     config["API_KEYS"] = {"OPEN_AI_API_KEY": "", "ANTHROPIC_API_KEY": ""}
-    config["USER_CFG"] = {"FIRST_USE": 1}
-    config["AI"] = {"ai": 'None'}
+    config["USER_CFG"] = {"FIRST_USE": 1, "MODEL": "", "ollama_installed": 0}
     home_dir = os.path.expanduser("~")
     config_file = os.path.join(home_dir, ".aicli_config.ini")
     with open(config_file, "w") as configfile:
