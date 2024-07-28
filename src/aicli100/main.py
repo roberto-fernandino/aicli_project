dc71260cee8017ad5d6a21e8680bf0ea7403b2c8
roberto-fernandino <romfernandino@gmail.com>
2024-07-28 15:38:42 -0300

    changed main location

---
+++ src/aicli100/main.py
@@ -0,0 +1,18 @@
+import curses
+from aicli100.cli import input_bar, initialize_screen
+
+
+
+def main(stdscr=None):
+    if stdscr is None:
+        return curses.wrapper(main)
+
+    initialize_screen(stdscr)
+    input_received = input_bar()
+    while input_received != "/exit":
+        input_received = input_bar()
+    
+
+
+if __name__ == "__main__":
+    curses.wrapper(main)
